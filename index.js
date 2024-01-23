22b58c33879b5cb5190888d86cea27b89acbc79c
martin.ostrat@khk.ee <martin.ostrat@khk.ee>
2024-01-23 20:18:34 +0200

    added express + handlebars

---
+++ index.js
@@ -0,0 +1,6 @@
+const app = require('./utils/app');
+const db = require('./utils/db');
+
+app.listen(30090, () => {
+    console.log('@30090');
+})
---
+++ utils/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + 'views/layouts'
+}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+module.exports = app;
