1507d3a538e4f3edd36a77b408f07fc18801e714
martin.ostrat@khk.ee <martin.ostrat@khk.ee>
2024-01-24 12:11:04 +0200

    As a user I can view a single yoga article [Delivers #186903998]

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require('./utils/app');
 const articleRoutes = require('./routes/articles');
 
 app.use('/', articleRoutes);
+app.use('/article', articleRoutes);
 
 app.listen(30090, () => {
     console.log('@30090');
